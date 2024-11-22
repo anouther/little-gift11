01f0495f934f9fe4bcc3989a06416d94ecc3c1dd
anouther <155570107+anouther@users.noreply.github.com>
2024-11-22 16:41:38 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'hearts-in-love page'.split('')
+const text = 'hearts inlove page'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
